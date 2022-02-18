7e2c70cda93a66781b39af5214c3a5dab3621a80
Artsiem Korbut <artsiem.korbut@softensity.com>
2022-02-18 18:34:53 +0300

    #20 updated attributes

--- MovieSite.WebAPI/Controllers/BaseApiController.cs
+++ MovieSite.WebAPI/Controllers/BaseApiController.cs
@@ -4,14 +4,15 @@ using MovieSite.Application.Models;
 
 namespace MovieSite.Controllers
 {
-    [ApiExplorerSettings(IgnoreApi = true)]
     public class BaseApiController : ControllerBase
     {
+        [NonAction]
         public ObjectResult CustomNotFound(ErrorCode? code)
         {
             return NotFound(new Error(code ?? ErrorCode.Unspecified));
         }
         
+        [NonAction]
         public ObjectResult CustomBadRequest(ErrorCode? code)
         {
             return BadRequest(new Error(code ?? ErrorCode.Unspecified));
