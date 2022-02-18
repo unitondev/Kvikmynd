49bed8c5a43ef6e6cf43fab6e4f9483b4f4db705
Artsiem Korbut <artsiem.korbut@softensity.com>
2022-02-18 15:22:45 +0300

    #20 Added errors messages

--- MovieSite.Application/Common/Enums/ErrorCode.cs
+++ MovieSite.Application/Common/Enums/ErrorCode.cs
@@ -2,26 +2,54 @@
 {
     public enum ErrorCode
     {
-        EntityNotCreated = 1000,
-        EntityNotUpdated = 1001,
-        EntityNotDeleted = 1002,
-        UserAlreadyExists = 1003,
-        UserNotFound = 1004,
-        UserNotCreated = 1005,
-        UserNotDeleted = 1006,
-        UserNotUpdated = 1007,
-        PasswordIsNotCorrect = 1008,
-        AccessTokenNotFound = 1009,
-        AccessTokenIsNotActive = 1010,
-        
-        MovieAlreadyExists = 1011,
-        MovieNotFound = 1012,
-        GenreNotFound = 1013,
-        MovieRatingNotFound = 1014,
-        MovieNotDeleted = 1015,
-        PasswordSpacesAtTheBeginningOrAtTheEnd = 1016,
-        ErrorWhileSettingRefreshToken = 1017,
-        RefreshTokenNotFound = 1018,
-        UserRatingNotFound = 1019,
+        #region General errors. Codes 1000-1099
+
+        Unauthorized = 1000,
+        NotFound = 1001,
+        ModelInvalid = 1002,
+        InternalServerError = 1003,
+        EntityNotCreated = 1004,
+        EntityNotUpdated = 1005,
+        EntityNotDeleted = 1006,
+
+        #endregion
+
+        #region User errors. Codes 1100-1199
+
+        UserNotFound = 1100,
+        UserNotCreated = 1101,
+        UserNotDeleted = 1102,
+        UserNotUpdated = 1103,
+        UserAlreadyExists = 1104,
+        PasswordIsNotCorrect = 1105,
+        AccessTokenNotFound = 1106,
+        AccessTokenIsNotActive = 1107,
+        PasswordSpacesAtTheBeginningOrAtTheEnd = 1108,
+        RefreshTokenNotFound = 1109,
+        ErrorWhileSettingRefreshToken = 1110,
+
+        #endregion
+
+        #region Movie errors. Codes 1200-1249
+
+        MovieAlreadyExists = 1200,
+        MovieNotFound = 1201,
+        MovieNotDeleted = 1202,
+
+        #endregion
+
+        #region Genre errors. Codes 1250-1299
+
+        GenreNotFound = 1250,
+
+        #endregion
+
+        #region Movie rating errors. Codes 1300-1349
+
+        MovieRatingNotFound = 1300,
+        UserRatingNotFound = 1301,
+
+        #endregion
+
     }
 }
---
+++ MovieSite.Application/Models/Error.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MovieSite.Application.Common.Enums;
+
+namespace MovieSite.Application.Models
+{
+    public class Error
+    {
+        [Required]
+        public int Code { get; set; }
+        [Required]
+        public string Message { get; set; }
+        
+        public Error(ErrorCode code)
+        {
+            Code = (int)code;
+            Message = GetDefaultMessageForErrorCode(code);
+        }
+        
+        public static string GetDefaultMessageForErrorCode(ErrorCode errorCode)
+        {
+            return errorCode switch
+            {
+                #region General errors. Codes 1000-1099
+                
+                ErrorCode.Unauthorized => "Unauthorized.",
+                ErrorCode.NotFound => "Resource not found.",
+                ErrorCode.ModelInvalid => "Invalid request model.",
+                ErrorCode.InternalServerError => "An unhandled exception thrown.",
+                ErrorCode.EntityNotCreated => "Entity wasn't created.",
+                ErrorCode.EntityNotUpdated => "Entity wasn't updated.",
+                ErrorCode.EntityNotDeleted => "Entity wasn't deleted.",
+                
+                #endregion
+                
+                #region User errors. Codes 1100-1199
+                
+                ErrorCode.UserNotFound => "User not found.",
+                ErrorCode.UserNotCreated => "User wasn't created.",
+                ErrorCode.UserNotDeleted => "User wasn't deleted.",
+                ErrorCode.UserNotUpdated => "User wasn't updated.",
+                ErrorCode.UserAlreadyExists => "User already exists.",
+                ErrorCode.PasswordIsNotCorrect => "User password is incorrect.",
+                ErrorCode.AccessTokenNotFound => "Access token not found.",
+                ErrorCode.AccessTokenIsNotActive => "Access token isn't active.",
+                ErrorCode.PasswordSpacesAtTheBeginningOrAtTheEnd => "There should be no spaces at the beginning and at the end of the password.",
+                ErrorCode.RefreshTokenNotFound => "Refresh token not found.",
+                ErrorCode.ErrorWhileSettingRefreshToken => "An error occurred while refresh token setting.",
+                
+                #endregion
+                
+                #region Movie errors. Codes 1200-1249
+                
+                ErrorCode.MovieAlreadyExists => "Movie already exists.",
+                ErrorCode.MovieNotFound => "Movie not found.",
+                ErrorCode.MovieNotDeleted => "Movie wasn't deleted.",
+                
+                #endregion
+                
+                #region Genre errors. Codes 1250-1299
+                
+                ErrorCode.GenreNotFound => "Genre not found.",
+                
+                #endregion
+                
+                #region Movie rating errors. Codes 1300-1349
+                
+                ErrorCode.MovieRatingNotFound => "Movie rating not found.",
+                ErrorCode.UserRatingNotFound => "User rating not found.",
+                
+                #endregion
+
+                _ => ""
+            };
+        }
+    }
+}
