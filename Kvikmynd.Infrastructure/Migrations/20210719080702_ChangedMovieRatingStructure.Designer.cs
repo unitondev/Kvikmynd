02e1df949429196b30574e582cccd9c702732560
Artsiem Korbut <artsiem.korbut@softensity.com>
2022-03-01 19:49:06 +0300

    #26 updated namespaces

--- Kvikmynd.Infrastructure/Migrations/20210719080702_ChangedMovieRatingStructure.Designer.cs
+++ Kvikmynd.Infrastructure/Migrations/20210719080702_ChangedMovieRatingStructure.Designer.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Kvikmynd.Infrastructure;
 
 namespace Kvikmynd.Infrastructure.Migrations
 {
