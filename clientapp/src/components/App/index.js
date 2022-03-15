1f8d772455a97d8225c2b799d510a2b3fc32ed85
Artsiem Korbut <artsiem.korbut@softensity.com>
2022-03-16 02:18:43 +0300

    Added redux-persist lib

--- clientapp/src/components/App/index.js
+++ clientapp/src/components/App/index.js
@@ -2,12 +2,16 @@ import React, { useEffect, } from 'react'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import UI from './App'
 import MUI from './Theme'
 import createStore, { history } from '../../state/createStore'
 import * as appActions from '../../state/actions'
 
 const store = createStore()
+const persistor = persistStore(store)
 
 const App = () => {
   useEffect(() => {
@@ -16,15 +20,17 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <React.StrictMode>
-        <MUI>
-          <div className='App'>
-            <ConnectedRouter history={history}>
-              <UI />
-            </ConnectedRouter>
-          </div>
-        </MUI>
-      </React.StrictMode>
+      <PersistGate persistor={persistor}>
+        <React.StrictMode>
+          <MUI>
+            <div className='App'>
+              <ConnectedRouter history={history}>
+                <UI />
+              </ConnectedRouter>
+            </div>
+          </MUI>
+        </React.StrictMode>
+      </PersistGate>
     </Provider>
   )
 }
