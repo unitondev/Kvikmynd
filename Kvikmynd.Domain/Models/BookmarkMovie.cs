d513c0402f679b411cef60bb01bb8237cdef7ce4
Artyom Korbut <artsiem.korbut@softensity.com>
2022-03-28 02:24:11 +0300

    #51 Added bookmark movie entity

---
+++ Kvikmynd.Domain/Models/BookmarkMovie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kvikmynd.Domain.Models
+{
+    public class BookmarkMovie
+    {
+        [Required]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        [Required]
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
